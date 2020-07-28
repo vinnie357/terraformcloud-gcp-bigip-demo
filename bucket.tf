5af20c41811a8a65ffc4f5c691bbe6ae5d358bd7
vinnie357 <vinnie.mazza@gmail.com>
2020-07-28 21:57:27 +0000

    add ha bucket

---
+++ bucket.tf
@@ -0,0 +1,12 @@
+resource google_storage_bucket bigip-ha {
+  name     = "${var.prefix}-bigip-storage"
+  location = "US"
+
+  website {
+    main_page_suffix = "index.html"
+    not_found_page   = "404.html"
+  }
+  labels = {
+      f5_cloud_failover_label = var.f5_cloud_failover_label
+  }
+}
